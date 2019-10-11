0d0bf431a03aaa3766338680b1756c53da484630
lucien dulac <lucien.dulac.externe@cnp.fr>
2019-10-11 16:30:27 +0200

    app: lazy load, use of sass, get data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
-import Scores from './components/Scores/Scores';
-import Vote from './components/Vote/Vote';
+import React, { lazy, Suspense } from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.scss';
+import api from './services/api/api';
+import mockCats from './__mock__/cats.json';
+
+const Welcome = lazy(() => import('./views/Welcome/Welcome'));
+const Scores = lazy(() => import('./views/Scores/Scores'));
+const Vote = lazy(() => import('./views/Vote/Vote'));
+
 
 function App() {
+  const [cats, setCatList] = useState([]);
+
+  useEffect(() => {
+    setCatList(mockCats.images || api.getCats());
+  }, []);
+
   return (
     <Router>
       <header className="App-header">
         <nav>
-          <Link to="/scores">Scores</Link>
-          <Link to="/vote">Voter</Link>
+          <NavLink to="/scores">Scores</NavLink>
+          <NavLink to="/vote">Vote</NavLink>
         </nav>
       </header>
-      <main>
+
+      <main className="App-main">
         <Switch>
-          <Route path="/scores">
-            <Scores />
+          <Route exact path="/">
+            <Suspense fallback={<h1>Loading Cutty App...</h1>}>
+              <Welcome />
+            </Suspense>
           </Route>
-          <Route path="/vote">
-            <Vote />
+          <Route exact path="/scores">
+            <Suspense fallback={<h1>Let's see actual scores</h1>}>
+              {
+                cats.length && <Scores cats={cats} />
+              }
+            </Suspense>
+          </Route>
+          <Route exact path="/vote">
+            <Suspense fallback={<h4>Now let's vote for the ONE</h4>}>
+              <Vote />
+            </Suspense>
           </Route>
         </Switch>
       </main>
