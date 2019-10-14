a98cfbf921a2b735404c8d065e593244f354f0d0
lucien dulac <lucien.dulac.externe@cnp.fr>
2019-10-15 00:08:03 +0200

    picture: performance upgrade

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import './Picture.scss';
 
-function Picture(props) {
+const Picture = memo(function Picture(props) {
   const { url } = props;
   return (
     <picture>
       <img src={url} alt=""/>
     </picture>
   );
-}
+});
 
 Picture.propTypes = {
   url: PropTypes.string
 };
+Picture.displayName = 'Picture';
 
 export default Picture;
