a72914d5c62a2df9a3f1d71fea46af6919f1745b
lucien dulac <lucien.dulac.externe@cnp.fr>
2019-10-16 12:03:39 +0200

    server: unecessary module removed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const stream = require('stream');
 
 const app = express();
 const port = process.env.APP_SERVER_PORT || 3000;
